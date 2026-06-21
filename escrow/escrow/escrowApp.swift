932fe4fdec396912b19cfc8fbbde7fa09d72164c
Anhelina <krukanhelina06@gmail.com>
2026-06-21 23:20:45 +0200

    chore: add Xcode project scaffold for iOS app

---
+++ escrow/escrow/escrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  escrowApp.swift
+//  escrow
+//
+//  Created by Anhelina  on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct escrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
